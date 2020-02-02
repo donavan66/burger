940bb547576fc8e2814bb0276372a7acdedb2961
Donavan Phothirath <donavan66@gmail.com>
2020-02-01 18:26:41 -0700

    added inital commit with all the code for turn in

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ("Cheeseburger");
+INSERT INTO burgers (burger_name) VALUES ("Beyond(ce) Burger");
+INSERT INTO burgers (burger_name) VALUES ("Astro Burger");
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,51 @@
+$(function() {
+  $(".change-eaten").on("click", function(event) {
+    var id = $(this).data("id");
+    var newEaten = $(this).data("neweaten");
+
+    var newEatenState = {
+      eaten: newEaten
+    };
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: newEatenState
+    }).then(
+      function() {
+        console.log("changed eaten to", newEaten);
+        location.reload();
+      }
+    );
+  });
+
+  $(".create-form").on("submit", function(event) {
+    event.preventDefault();
+
+    var newBurgers = {
+      name: $("#ca").val().trim(),
+      eaten: $("[name=eaten]:checked").val().trim()
+    };
+
+    // Send the POST request.
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurgers
+    }).then(
+      function() {
+        console.log("created new burger");
+        location.reload();
+      }
+    );
+  });
+
+  $(".delete-burger").on("click", function(event) {
+    var id = $(this).data("id");
+    $.ajax("/api/burgers/" + id, {
+      type: "DELETE"
+    }).then(
+      function() {
+        console.log("deleted burger", id);
+        location.reload();
+      }
+    );
+  });
+});
